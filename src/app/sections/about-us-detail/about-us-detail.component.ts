f56d3897c7d9415848f440779193cd86cf26f4e5
jrferguson <1q2w3e4r5t6y>
2023-05-19 00:29:22 +0100

    added about us

--- src/app/sections/about-us-detail/about-us-detail.component.ts
+++ src/app/sections/about-us-detail/about-us-detail.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-about-us-detail',
-  templateUrl: './about-us-detail.component.html',
-  styleUrls: ['./about-us-detail.component.scss']
+    selector: 'app-about-us-detail',
+    templateUrl: './about-us-detail.component.html',
+    styleUrls: ['./about-us-detail.component.scss'],
 })
 export class AboutUsDetailComponent implements OnInit {
+    constructor(private router: Router) {}
 
-  constructor() { }
+    ngOnInit() {}
 
-  ngOnInit() {
-  }
-
+    onRedirect(section: string) {
+        this.router.navigate([section]);
+    }
 }
