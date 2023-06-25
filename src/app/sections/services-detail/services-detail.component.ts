19ee04f6829276c32f7e6ec250f9db3f33a3ccf3
jrferguson <1q2w3e4r5t6y>
2023-06-25 13:34:15 +0100

    update prices

--- src/app/sections/services-detail/services-detail.component.ts
+++ src/app/sections/services-detail/services-detail.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import {
+    PACKAGE_1_PRICE_AFTER_DISCOUNT,
+    PACKAGE_1_PRICE_BEFORE_DISCOUNT,
+    PACKAGE_2_PRICE_AFTER_DISCOUNT,
+    PACKAGE_2_PRICE_BEFORE_DISCOUNT,
+} from 'src/constant';
 
 @Component({
     selector: 'app-services-detail',
@@ -7,6 +13,11 @@ import { Router } from '@angular/router';
     styleUrls: ['./services-detail.component.scss'],
 })
 export class ServicesDetailComponent implements OnInit {
+    package1BeforeDiscount = PACKAGE_1_PRICE_BEFORE_DISCOUNT;
+    package1AfterDiscount = PACKAGE_1_PRICE_AFTER_DISCOUNT;
+    package2BeforeDiscount = PACKAGE_2_PRICE_BEFORE_DISCOUNT;
+    package2AfterDiscount = PACKAGE_2_PRICE_AFTER_DISCOUNT;
+
     constructor(private router: Router) {}
 
     ngOnInit() {}
